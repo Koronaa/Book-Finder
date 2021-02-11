9e0af00730680f2f350379bb61834fce70a3c45f
Sajith Konara <konara@keeneye.solutions>
2021-02-11 17:18:16 +0530

    Implements project structure

---
+++ Book Finder/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Book Finder
+//
+//  Created by Sajith Konara on 2021-02-11.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Book Finder/SupportingFiles/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  Book Finder
+//
+//  Created by Sajith Konara on 2021-02-11.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+}
+
